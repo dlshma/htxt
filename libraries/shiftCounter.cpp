8e473e2ddf5c3af91e843222c25da71857a85040
dlshma <199683390+dlshma@users.noreply.github.com>
2025-02-18 13:57:14 -0500

    Reworked shiftCounter.cpp

--- libraries/shiftCounter.cpp
+++ libraries/shiftCounter.cpp
@@ -5,15 +5,8 @@
 #include <cstdint>
 using namespace std;
 
-// example bytes to showcase functionality
-uint8_t a{0b01100100};
-uint8_t b{0b11101110};
-uint8_t c{0b01100101};
-
-uint8_t bytes[] {a, b, c};
-
 // global count variables (this will break so make it not global plz)
-int count{0};
+int global_count{};
 int globalx{0};
 
 // returns bit in counter byte
@@ -25,8 +18,8 @@ int checkBit(int x, int y)
 
 // uUGHH
 // pass in the target byte, current 'value' to reference if shift occurred, and the bit being referenced
-void shift(int target, int shiftval, int spot)
-{       
+void shift(int target, int shiftval, int spot, int count)
+{ 
     if (spot < 8) 
     {
         int x = checkBit(target, spot);
@@ -35,32 +28,23 @@ void shift(int target, int shiftval, int spot)
             shiftval = x; 
             cout << "switched after " << count << " bit(s)\n"; 
             count = 1;
-            shift(target, shiftval, ++spot);
+            shift(target, shiftval, ++spot, count);
         }
         else { // IF THERE IS NO CHANGE: move it along
             count++; 
-            shift(target, shiftval, ++spot);
+            shift(target, shiftval, ++spot, count);
         }
     } else {
-		globalx = shiftval;
-	}
-}
-
-// checkByte() is just to print the whole byte so you can see it OHHH THAT's WHY checkBit() IS SEPARATE
-void checkByte(int z) 
-{
-    for (int i{}; i < 8; i++) {
-        cout << checkBit(z, i);
-        "/n";
+        globalx = shiftval;
+        global_count = count;
+        cout << "reached end of byte with " << global_count << " remaining bit(s) (value " << globalx << ")\n";
     }
 }
 
-int main() 
+// checkByte() is just to print the whole byte so you can see it OHHH THAT's WHY checkBit() IS SEPARATE
+void checkByte(int z)
 {
-	for (int i : bytes)
-	{
-		shift(i, globalx, 0);
-		cout << "reached end of byte with " << count << " remaining bit(s) (value " << globalx << ")\n";
-	}
-    return 0;
+    for (int i{}; i < 8; i++) {
+        cout << checkBit(z, i) << "/n";
+    }
 }
